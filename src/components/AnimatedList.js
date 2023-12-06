54f735e3cb3eb5d52431e38f72fe804c9f6975be
harperrose <101375619+harperrose@users.noreply.github.com>
2023-12-05 23:36:31 -0500

    list animation that works
    style updates too. going to switch height to scale next

--- src/components/AnimatedList.js
+++ src/components/AnimatedList.js
@@ -8,24 +8,28 @@ const AnimatedListItem = ({ project }) => {
     height: '100px',
     blur: 30,
   }));
-  
+
   const handleScroll = useCallback(() => {
     const rect = ref.current.getBoundingClientRect();
-    const scrollPosition = window.scrollY || window.pageYOffset;
     const viewportHeight = window.innerHeight;
-    const fiftyPercentPoint = viewportHeight * 0.5;
-    const seventyFivePercentPoint = viewportHeight * 0.75;
-  
-    const isInsideRange = scrollPosition >= rect.top + fiftyPercentPoint && scrollPosition <= rect.top + seventyFivePercentPoint;
-  
-    if (scrollPosition >= rect.top && scrollPosition <= rect.bottom) {
-      set((prev) => ({
-        height: isInsideRange ? '200px' : '100px',
-        blur: isInsideRange ? 0 : 30,
-      }));
+
+    const isInsideViewport = rect.top < viewportHeight && rect.bottom >= 0;
+
+    if (isInsideViewport) {
+      const scrollPosition = window.scrollY || window.pageYOffset;
+      const startAnimationPosition = rect.top + viewportHeight * 0.5;
+      const endAnimationPosition = rect.top + viewportHeight * 0.75;
+
+      set({
+        height:
+          scrollPosition >= startAnimationPosition && scrollPosition <= endAnimationPosition
+            ? '200px'
+            : '100px',
+        blur:
+          scrollPosition >= startAnimationPosition && scrollPosition <= endAnimationPosition ? 0 : 30,
+      });
     }
   }, [set]);
-  
 
   useEffect(() => {
     handleScroll(); // Initial check
@@ -37,19 +41,19 @@ const AnimatedListItem = ({ project }) => {
   }, [handleScroll]);
 
   return (
-    <animated.li ref={ref} style={{ height: animationProps.height, overflow: 'hidden' }}>
-      <a href= "{project.link}"  className="animated-list-item">
-         <h2>{project.name}</h2>
-        <animated.img
-          className="animated-list-image"
-          style={{
-            filter: animationProps.blur.interpolate((b) => `blur(${b}px)`),
-          }}
-          src={process.env.PUBLIC_URL + '/images/' + project.image1}
-          alt={project.name}
-        />
-      </a>
-    </animated.li>
+    <animated.li ref={ref} style={{ height: animationProps.height }}>
+    <a href= "{project.link}"  className="animated-list-item">
+       <h2>{project.name}</h2>
+      <animated.img
+        className="animated-list-image"
+        style={{
+          filter: animationProps.blur.interpolate((b) => `blur(${b}px)`),
+        }}
+        src={process.env.PUBLIC_URL + '/images/' + project.image1}
+        alt={project.name}
+      />
+    </a>
+  </animated.li>
   );
 };
 
