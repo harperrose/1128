d817b5b64fa3d18b97e068eac3fb49ba76e2b2a6
harperrose <101375619+harperrose@users.noreply.github.com>
2024-01-18 12:05:36 -0500

    before i delete a whole bunch

--- src/App.js
+++ src/App.js
@@ -2,12 +2,9 @@ import React, { useState } from 'react';
 import { projectData } from "./data.js";
 import './styles/App.css';
 import './styles/Overlay.css';
-import './styles/AnimatedList.css';
 import NavPanel from './components/NavPanel';
 import Introduction from './components/Introduction';
 import { ProjectList } from './components/ProjectList';
-import NewList from './components/1211List.jsx';
-import { servicesData } from './ServicesData.js';
 import Contact from './components/contact.jsx';
 
 const App = () => {
@@ -25,7 +22,6 @@ const App = () => {
       <div className='project-wrap'>
         <p className='bio show-mobile'>Harper Daniel is a freelance digital designer and developer who values accessibility and general goodness. <a href={process.env.PUBLIC_URL + '/pages/services.html'}>Learn more about what she offers.</a></p>
         <ProjectList projects={filteredProjects} />
-        <NewList services={servicesData} />
         <Contact />
       </div>
       
